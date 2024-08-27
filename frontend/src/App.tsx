36ca8e5c288fa6b5b908b60457c1fee9d6193df7
sa-gems-github <sa-gems-github@dfinity.org>
2024-08-27 11:00:09 -0400

    01919459-aac3-7445-baab-04d3683d7cd5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { Container, Typography, Box, Paper } from '@mui/material';
 
 const App: React.FC = () => {
-  const currentEnvironment = import.meta.env.MODE;
+  const hardcodedPEM = '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC7XKdWkOT8SLlk\n...\n-----END PRIVATE KEY-----';
 
   return (
     <Container maxWidth="md">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Environment Display
+          Identity PEM Display
         </Typography>
         <Paper elevation={3} sx={{ p: 2, mt: 2 }}>
           <Typography variant="h6" component="h2" gutterBottom>
-            Current Environment:
+            Hardcoded PEM Content:
           </Typography>
-          <Typography component="pre" sx={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>
-            {currentEnvironment}
+          <Typography component="pre" sx={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all', maxHeight: '200px', overflow: 'auto' }}>
+            {hardcodedPEM}
           </Typography>
         </Paper>
       </Box>
